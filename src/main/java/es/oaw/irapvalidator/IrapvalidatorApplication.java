0e438c11f530cc513944d7548321fd2dc2259337
Álvaro Peláez <alvaro.pelaez@fundacionctic.org>
2020-11-05 12:03:23 +0100

    Added apj configuration

--- src/main/java/es/oaw/irapvalidator/IrapvalidatorApplication.java
+++ src/main/java/es/oaw/irapvalidator/IrapvalidatorApplication.java
@@ -1,7 +1,10 @@
 package es.oaw.irapvalidator;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
@@ -32,5 +35,26 @@ public class IrapvalidatorApplication {
 		multipartResolver.setDefaultEncoding("UTF-8");
 		multipartResolver.setPreserveFilename(true);
 		return multipartResolver;
+	}
+
+	/**
+	 * Servlet container.
+	 *
+	 * @return the tomcat servlet web server factory
+	 */
+	@Bean
+	public TomcatServletWebServerFactory servletContainer() {
+
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+
+		Connector ajpConnector = new Connector("AJP/1.3");
+		ajpConnector.setPort(8091);
+		ajpConnector.setSecure(false);
+		ajpConnector.setAllowTrace(false);
+		ajpConnector.setScheme("http");
+		((AbstractAjpProtocol) ajpConnector.getProtocolHandler()).setSecretRequired(false);
+		tomcat.addAdditionalTomcatConnectors(ajpConnector);
+
+		return tomcat;
 	}
 }
